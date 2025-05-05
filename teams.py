eaa7cb0961d2ae86068f39801e772efc6fc7e79d
dvanheeswijk <52838748+dvanheeswijk@users.noreply.github.com>
2025-05-05 15:06:45 -0400

    Create teams.py

---
+++ teams.py
@@ -0,0 +1,11 @@
+import streamlit
+import pandas as pd
+import numpy as np
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+TEAMS_URL = os.getenv("TEAMS_URL")
+
+print("Hello, world!")
