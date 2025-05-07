fd4973aa6332c770bef5ba17c6d3ccc7fd80fdb0
dvanheeswijk <52838748+dvanheeswijk@users.noreply.github.com>
2025-05-07 11:17:46 -0400

    Update teams.py

--- teams.py
+++ teams.py
@@ -4,6 +4,7 @@ import numpy as np
 from dotenv import load_dotenv
 import os
 import pymsteams
+import asyncio
 import requests
 
 load_dotenv()
@@ -11,23 +12,23 @@ load_dotenv()
 TEAMS_URL = os.getenv("TEAMS_URL")
 
 def send_alert_to_teams(message, webhook_url):
+  loop = asyncio.get_event_loop()
   # Instantiate the connector card object
-  teams_message = pymsteams.connectorcard(webhook_url)
+  # teams_message = pymsteams.connectorcard(webhook_url)
+  # the async_connectorcard object is used instead of the normal one.
+  teams_message = pymsteams.async_connectorcard(webhook_url)
   # Set the text of the message
   teams_message.text(message)
   # Publish the message to the MS Teams channel
-  teams_message.send()
+  loop.run_until_complete(teams_message.send())
 
 st.write("""
 #Test
 This is testing the streamlit webhook using a url in github secrets.
 """)
 
-# message = "This is a test of streamlit with webhook."
 
-# i = 0
-# if i<1:
-#   send_alert_to_teams(message, TEAMS_URL)
-#   i=i+1
+send_alert_to_teams(st, TEAMS_URL)
 
-print(st)
+
+print("Done!")
